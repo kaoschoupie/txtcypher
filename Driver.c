14c34269dd024eeeaac4366da721b97d7fad66b7
kaoschoupie <maxime9212@gmail.com>
2025-01-16 18:53:34 -0500

    finished it

--- Driver.c
+++ Driver.c
@@ -2,28 +2,47 @@
 #include "Decypher.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-    FILE *toread = fopen("./tocypher.txt", "r");
-    FILE *towrite = fopen("cyphered.txt", "w");
+    if (argc < 2)
+    {
+        printf("Please enter enough arguments");
+        exit(0);
+    }
+    FILE *toread = fopen(argv[2], "r");
     char line[256];
-    while (fgets(line, sizeof(line), toread))
+    if (strcmp(argv[1], "cypher") == 0)
     {
-        char *str = cypher(line, sizeof(line), 1);
-        fprintf(towrite, str);
-        free(str);
+        printf("Entered cypher mode");
+        // FILE *toread = fopen("./tocypher.txt", "r");
+        FILE *towrite = fopen("cyphered.txt", "w");
+        // char line[256];
+        while (fgets(line, sizeof(line), toread))
+        {
+            char *str = cypher(line, sizeof(line), 1);
+            fprintf(towrite, str);
+            free(str);
+        }
+        fclose(toread);
+        fclose(towrite);
+        remove(argv[2]);
     }
-    fclose(toread);
-    fclose(towrite);
-    toread = fopen("cyphered.txt", "r");
-    towrite = fopen("decyphered.txt", "w");
-    while (fgets(line, sizeof(line), toread))
+    if (strcmp(argv[1], "decypher") == 0)
     {
-        char *str = decypher(line, sizeof(line), 1);
-        fprintf(towrite, str);
-        free(str);
+        printf("Entered decypher mode");
+        // FILE *toread = fopen("cyphered.txt", "r");
+        FILE *towrite = fopen("decyphered.txt", "w");
+        // char line[256];
+        while (fgets(line, sizeof(line), toread))
+        {
+            char *str = decypher(line, sizeof(line), 1);
+            fprintf(towrite, str);
+            free(str);
+        }
+        fclose(toread);
+        fclose(towrite);
+        remove(argv[2]);
     }
-    fclose(toread);
-    fclose(towrite);
 }
