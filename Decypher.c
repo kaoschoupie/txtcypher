a55908ba7c4067ae4bdcd71084b4edd14fff3028
kaoschoupie <160160126+kaoschoupie@users.noreply.github.com>
2025-01-17 14:34:38 -0500

    added termination character detection (#1)

--- Decypher.c
+++ Decypher.c
@@ -6,7 +6,7 @@ char *decypher(char *str, int size, int key)
     char *output = (char *)malloc((size + 1) * sizeof(char));
     for (int i = 0; i < size; i++)
     {
-        if (str[i] == '\n')
+        if (str[i] == '\n' || str[i] == '\0')
         {
             output[i] = '\n';
             output[i + 1] = '\0';
